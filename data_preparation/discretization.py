c640e8b42aac503c41696a02fddf03a50958c1ab
Marius Hegele <marius.hegele@ibm.com>
2020-04-05 10:18:07 +0200

    Restructure and add discretize volume deviation

--- data_preparation/discretization.py
+++ data_preparation/discretization.py
@@ -4,15 +4,18 @@ def discretize(df):
         'tonlage': (205, 423),
         'schallstaerke': (25, 49)
     }
-    
+
     def _stringify(x, lower, upper):
         if x <= lower: return "low"
         if x >= upper: return "high"
         return "normal"
-    
+
     for col in df:
         lower_border, upper_border = borders[col]
-        
+
         df[col] = df[col].map(lambda x: _stringify(x, lower_border, upper_border))
-    
+
     return df
+
+def discretize_volume_deviation(window):
+    return window.std() > 12.2
